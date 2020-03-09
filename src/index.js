e9eac3266698608f083151f04320029c4bc9b40a
BarbedWire <e.barbedwire@gmail.com>
2020-03-09 10:31:04 +0300

    feat: solve typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array !== undefined && Array.isArray(array) && array.length) {
+    return Math.min(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array !== undefined && Array.isArray(array) && array.length) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array !== undefined && Array.isArray(array) && array.length) {
+    return array.reduce((a, e) => a + e, 0) / array.length;
+  } else {
+    return 0;
+  }
 }
+
+//console.log(min([2,-24,13,-11,16,-12,-16,24,13,-38,19,-16,19,3,2,32,0,-10,-19,-27]));
